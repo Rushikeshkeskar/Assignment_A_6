c50612b62081640045560e82433b60209b8e58d9
Rushikeshkeskar <81505552+Rushikeshkeskar@users.noreply.github.com>
2021-05-30 20:07:38 +0530

    Add files via upload

---
+++ Assignment 6.php
@@ -0,0 +1,16 @@
+<?xml version='1.0' encoding='UTF-8' ?>
+<CricketTeam>
+            <Team Country="India">
+			             <player>Virat Kohli</player>
+						 <wickets>4</wickets>
+						 <runs>22000</runs>
+	
+            </Team>
+
+            <Team Country="Newzeland">
+			             <player>Kane Williamsan</player>
+						 <wickets>18</wickets>
+						 <runs>15000</runs>
+	
+            </Team>
+</CricketTeam>
